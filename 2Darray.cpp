5ff335c69806fdde8d79a529e17949e66ca4450d
Tanisha Yadav <24csu214@ncuindia.edu>
2025-02-03 17:19:26 +0530

    Add files via upload

---
+++ 2Darray.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[3][3],i,j;
+    cout<<"provide array elements";
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            cin>>arr[j][i];
+        }
+    }
+    cout<<"provided array looks like"<<endl;
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            cout<<"\t"<<arr[i][j];
+        }
+        cout<<endl;
+    }
+
+}
