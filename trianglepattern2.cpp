33f48af23b65dba591c2ba5871e98b6144cd507e
Tanisha Yadav <24csu214@ncuindia.edu>
2025-05-30 08:24:00 +0530

    Add files via upload

---
+++ trianglepattern2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,n;
+    n=4;
+    for(i=0;i<n;i++){
+        for(j=0;j<=i;j++){
+            cout<<i+1<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
